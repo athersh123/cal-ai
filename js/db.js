eb2d7a7a0ff5b0114a9e66b396849e7fbdcd1d02
athersh123 <athersh124@gmail.com>
2026-06-04 14:47:26 +0530

    Added environment variables

--- js/db.js
+++ js/db.js
@@ -1,16 +1,47 @@
+// // import mysql from "mysql2";
+
+// // const db = mysql.createConnection({
+// //   // host: "localhost",
+// //   // user: "root",
+// //   // password: "ather@333s",
+
+// //   database: "nutrivision",
+// //   port: 3306
+// // });
+
+// import mysql from "mysql2";
+
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT || 3306
+// });
+
+// export default db;
+// db.connect((err) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log("MySQL Connected");
+// });
+
+// export default db;
 import mysql from "mysql2";
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "ather@333s",
-  database: "nutrivision",
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
   if (err) {
-    console.error(err);
+    console.error("MySQL Connection Error:", err);
     return;
   }
   console.log("MySQL Connected");
